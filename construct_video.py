4c0822caa6eaf9ca60f8b029b4286b8bd438421d
Petr <petaschlup@seznam.cz>
2023-08-26 01:54:56 +0200

    WIP: Pipeline experimentr

--- construct_video.py
+++ construct_video.py
@@ -1,33 +1,60 @@
 import cv2
 
 
-def create_video_from_frames(frame_paths, output_video_path, fps=10):
-    try:
+# def create_video_from_frames(frame_paths, output_video_path, fps=10):
+#     try:
+#         frame_size = None
+#         out = None
+#         frame_number = 0  # Initialize frame number
+#
+#         fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+#
+#         for frame_path in frame_paths:
+#             img = cv2.imread(frame_path[0])
+#
+#             if frame_size is None:
+#                 frame_size = (img.shape[1], img.shape[0])
+#                 out = cv2.VideoWriter(output_video_path, fourcc, fps, frame_size)
+#
+#             # Add frame number label onto the frame
+#             label = f'Frame: {frame_number}'
+#             cv2.putText(img, label, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
+#
+#             out.write(img)
+#             frame_number += 1  # Increment frame number
+#
+#         if out is not None:
+#             out.release()
+#
+#         return True, output_video_path
+#
+#     except Exception as e:
+#         print(e)
+#         return False, None
+
+def create_video_from_frames(frame_generator, output_video_path, fps=10):
+
+    for frame in frame_generator:
         frame_size = None
         out = None
         frame_number = 0  # Initialize frame number
 
         fourcc = cv2.VideoWriter_fourcc(*'mp4v')
 
-        for frame_path in frame_paths:
-            img = cv2.imread(frame_path[0])
+        img = frame.frame
 
-            if frame_size is None:
-                frame_size = (img.shape[1], img.shape[0])
-                out = cv2.VideoWriter(output_video_path, fourcc, fps, frame_size)
+        if frame_size is None:
+            frame_size = (img.shape[1], img.shape[0])
+            out = cv2.VideoWriter(output_video_path, fourcc, fps, frame_size)
 
-            # Add frame number label onto the frame
-            label = f'Frame: {frame_number}'
-            cv2.putText(img, label, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
+        # Add frame number label onto the frame
+        label = f'Frame: {frame_number}'
+        cv2.putText(img, label, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
 
-            out.write(img)
-            frame_number += 1  # Increment frame number
+        out.write(img)
+        frame_number += 1  # Increment frame number
 
-        if out is not None:
-            out.release()
+    if out is not None:
+        out.release()
 
-        return True, output_video_path
-
-    except Exception as e:
-        print(e)
-        return False, None
+    return True, output_video_path
