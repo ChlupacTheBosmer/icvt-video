52dc4eb0bccf560ce2abd5ec1b4e0faf84b4e71b
Petr <petaschlup@seznam.cz>
2023-08-25 09:55:50 +0200

    MTC General Data Pipeline Step 3 integration.

--- construct_video.py
+++ construct_video.py
@@ -1,25 +1,32 @@
 import cv2
 
 def create_video_from_frames(frame_paths, output_video_path, fps=10):
-    frame_size = None
-    out = None
-    frame_number = 0  # Initialize frame number
 
-    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    try:
+        frame_size = None
+        out = None
+        frame_number = 0  # Initialize frame number
 
-    for frame_path in frame_paths:
-        img = cv2.imread(frame_path[0])
+        fourcc = cv2.VideoWriter_fourcc(*'mp4v')
 
-        if frame_size is None:
-            frame_size = (img.shape[1], img.shape[0])
-            out = cv2.VideoWriter(output_video_path, fourcc, fps, frame_size)
+        for frame_path in frame_paths:
+            img = cv2.imread(frame_path[0])
 
-        # Add frame number label onto the frame
-        label = f'Frame: {frame_number}'
-        cv2.putText(img, label, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
+            if frame_size is None:
+                frame_size = (img.shape[1], img.shape[0])
+                out = cv2.VideoWriter(output_video_path, fourcc, fps, frame_size)
 
-        out.write(img)
-        frame_number += 1  # Increment frame number
+            # Add frame number label onto the frame
+            label = f'Frame: {frame_number}'
+            cv2.putText(img, label, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
 
-    if out is not None:
-        out.release()
+            out.write(img)
+            frame_number += 1  # Increment frame number
+
+        if out is not None:
+            out.release()
+
+        return True, output_video_path
+
+    except:
+        return False, None
